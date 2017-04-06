4fe176f766a0b38f87be374741c9ff8c8b18fd7d
Cristina BAUTISTA <c.bautista@jeuneafrique.com>
2017-04-06 17:27:39 +0200

    no message

--- manipulations-ajax/app.js
+++ manipulations-ajax/app.js
@@ -17,14 +17,19 @@ var website = website || {}; // Si « website » a déjà été crée dans un pr
 
     publics.getArticleJA = function(){
       var url = 'https://www.jeuneafrique.com/api/business/v1.0/homepage/';
-      var textBtnAction = $("#my-action-btn").text();
+      var $fluxContainer = $("#result-ajax-1");
+      var $resultDiv = $fluxContainer.find(' .result-ajax-body');
+      var $btn = $resultDiv.find('.btn');
+      var $btnSpan = $resultDiv.find('.btn .my-action-text');
+      var $textBtn = $btnSpan.text();
       var request = $.ajax({
         url: url,
         method: "GET",
         dataType: "json",
         cache: true,
         beforeSend: function() {
-          $("#my-action-btn").text('Chargement'); // On indique sur le bouton que le chargement est en cours
+          $resultDiv.find($btn).addClass('disabled');
+          $resultDiv.find($btnSpan).text('Chargement'); // On indique sur le bouton que le chargement est en cours
         }
       });
 
@@ -40,7 +45,7 @@ var website = website || {}; // Si « website » a déjà été crée dans un pr
           $( "<ul/>", {
           "class": "my-new-list",
           html: items.join( "" )
-          }).appendTo( "#result-ajax" );
+          }).appendTo( $resultDiv );
 
       });
 
@@ -50,16 +55,38 @@ var website = website || {}; // Si « website » a déjà été crée dans un pr
 
       request.always(function() {
           console.log( "complete" );
-          $("#my-action-btn").text(textBtnAction); // Quel que soit le résultat (done ou fail) on remet le texte initial du bouton
+          // Quel que soit le résultat (done ou fail) on remet l'état initial du bouton
+          $btn.removeClass('disabled');
+          $btnSpan.text($textBtn);
       });
     }
 
-    // Variante avec methode getJSON
-    publics.getArticleJA_2 = function(){
-      $.getJSON("https://www.jeuneafrique.com/api/business/v1.0/homepage/", function( response ){
-          var items = [];
-          $.each( response.data.posts, function(i, post){
-              var title = post.title;
+
+
+    publics.getArticleGrandParis = function(){
+      var url = 'https://www.lejournaldugrandparis.fr/wp-json/wp/v2/posts';
+      var $fluxContainer = $("#result-ajax-2");
+      var $resultDiv = $fluxContainer.find(' .result-ajax-body');
+      var $btn = $resultDiv.find('.btn');
+      var $btnSpan = $resultDiv.find('.btn .my-action-text');
+      var $textBtn = $btnSpan.text();
+      var request = $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "json",
+        cache: true,
+        beforeSend: function() {
+          $resultDiv.find($btn).addClass('disabled');
+          $resultDiv.find($btnSpan).text('Chargement'); // On indique sur le bouton que le chargement est en cours
+        }
+      });
+
+      // Ci-dessous exemple avec les promises (done, fail, always)
+      request.done(function(response) {
+        console.log(response);
+        var items = [];
+          $.each( response, function(i, post){
+              var title = post.links;
               var artId = post.id;
               console.log(i , post);
               items.push("<li id='article_"+ i +"'><h4>"+title+"</b> - <small>"+ artId +"</small></h4></li>");
@@ -67,17 +94,52 @@ var website = website || {}; // Si « website » a déjà été crée dans un pr
           $( "<ul/>", {
           "class": "my-new-list",
           html: items.join( "" )
-          }).appendTo( "#result-ajax" );
+          }).appendTo( $resultDiv );
+
+      });
+
+      request.fail(function( jqXHR, textStatus ) {
+        console.error( "Request failed: " + textStatus );
+      });
+
+      request.always(function() {
+          console.log( "complete" );
+          // Quel que soit le résultat (done ou fail) on remet l'état initial du bouton
+          $btn.removeClass('disabled');
+          $btnSpan.text($textBtn);
       });
     }
+
+    // Variante avec methode getJSON
+    // publics.getArticleJA = function(){
+    //   $.getJSON("https://www.jeuneafrique.com/api/business/v1.0/homepage/", function( response ){
+    //       var items = [];
+    //       $.each( response.data.posts, function(i, post){
+    //           var title = post.title;
+    //           var artId = post.id;
+    //           console.log(i , post);
+    //           items.push("<li id='article_"+ i +"'><h4>"+title+"</b> - <small>"+ artId +"</small></h4></li>");
+    //       });
+    //       $( "<ul/>", {
+    //       "class": "my-new-list",
+    //       html: items.join( "" )
+    //       }).appendTo( "#result-ajax-1" );
+    //   });
+    // }
 
     publics.manageEvents = function(){
 
           // Click event on id element
-          $("#my-action-btn").on('click', function(e) {
+          $("#result-ajax-1").on('click', '.btn', function(e) {
             e.preventDefault();
 
             publics.getArticleJA();
+          });
+
+          $("#result-ajax-2").on('click', '.btn', function(e) {
+            e.preventDefault();
+
+            publics.getArticleGrandParis();
           });
     }
 
